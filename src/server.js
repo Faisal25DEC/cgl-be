f723efef389c188cccf9ecbaa1c46ddbf4dd98cb
Muhammad Faisal <faisalmd25121999@gmail.com>
2025-10-26 14:28:37 +0530

    fdas

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 // If you need credentials/cookies, set a specific origin and credentials: true
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://dev-cgl-fe.vercel.app"],
+    origin: ["http://localhost:5173", "https://cgl-fe.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
